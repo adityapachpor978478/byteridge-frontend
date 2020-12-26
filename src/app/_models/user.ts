412ce9499c69169e884bd1703c20b4f1c9476ee4
unknown <ap00556767@techmahindra.com>
2020-12-26 19:55:57 +0530

    added initial changes

---
+++ src/app/_models/user.ts
@@ -0,0 +1,12 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+    loginTime: string;
+    logoutTime: string;
+    clientIp: string;
+    role: string;
+}
---
+++ src/app/audit/audit.component.ts
@@ -0,0 +1,36 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@/_models';
+import { UserService, AuthenticationService } from '@/_services';
+
+@Component({ templateUrl: 'audit.component.html' })
+export class AuditComponent implements OnInit {
+    currentUser: User;
+    users = [];
+
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    columnDefs = [
+        {headerName: "Role", field: "role", sortable: true, filter: true},
+        {headerName: "Username", field: "username", sortable: true, filter: true},
+        {headerName: "Login Time", field: "loginTime", sortable: true, filter: true},
+        {headerName: "Logout Time", field: "logoutTime", sortable: true, filter: true},
+        {headerName: "Client IP", field: "clientIp", sortable: true, filter: true}
+    ];
+
+    ngOnInit() {
+        this.loadAuditData();
+    }
+
+    private loadAuditData() {
+        this.userService.getAudit()
+            .pipe(first())
+            .subscribe(users => this.users = users);
+    }
+}
